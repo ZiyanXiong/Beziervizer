61b6590721db31d7750601b90071ba73820b2d21
Ziyan Xiong <ziyanxiong97@gmail.com>
2021-11-22 22:11:04 -0600

    Add document for function plot_cubic_bezier

--- R/Cubic_Bezier_Curve.R
+++ R/Cubic_Bezier_Curve.R
@@ -1,10 +1,12 @@
 
-
-#' Title
+#' @title Plot a cubic Bezier curve
+#'
+#' @description This function will lunch a Shiny app for plotting an interactive Bezier curve. The slider t controls the animation of De Casteljau's algorithm. The coordinates of each control points will be printed out on each movement.
 #'
 #' @export
 #'
 #' @examples
+#' #plot_cubic_bezier()
 plot_cubic_bezier <- function()
 {
   shiny::runApp(appDir = system.file("Slider", package = "Beziervizer"))
