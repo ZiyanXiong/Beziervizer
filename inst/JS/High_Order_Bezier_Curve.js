6e4ee853f0088431216ef888b46ecd20ff0a0b34
Ziyan Xiong <ziyanxiong97@gmail.com>
2021-11-26 13:36:43 -0600

    Add code for combining high order beizer curve plot with a silder to contol iterate times

--- inst/JS/High_Order_Bezier_Curve.js
+++ inst/JS/High_Order_Bezier_Curve.js
@@ -5,11 +5,21 @@
 
   const radius = 6;
   var iter_times = 5;
-  var circles = d3.range(25).map(i => ({
+  var circles = d3.range(20).map(i => ({
     x: Math.random() * (width - radius * 2) + radius,
     y: Math.random() * (height - radius * 2) + radius,
   }));
 
+  var margin = {right: 50, left: 50};
+  width = svg.attr("width") - margin.left - margin.right;
+  height = svg.attr("height");
+  var timer;
+  var x = d3.scaleLinear()
+    .domain([3, 10])
+    .range([0, width])
+    .clamp(true);
+
+  var handle_position = [{x: (iter_times - 3) / 7 * width, y:0}];
 
   if(data.length >= 4){
     circles = data;
@@ -35,10 +45,15 @@
   function update() {
     sub_points = recursive_subdivision(circles, iter_times);
     flattened = sub_points.reduce(function(a, b) { return a.concat(b);},[]);
+    svg.select("#subdivision").selectAll("circle").remove();
+
     svg.select("#subdivision").selectAll("circle")
-        .data(flattened)
-        .attr("cx", d => d.x)
-        .attr("cy", d => d.y)
+    .data(flattened)
+    .join("circle")
+      .attr("cx", d => d.x)
+      .attr("cy", d => d.y)
+      .attr("r", 2.5)
+      .attr("fill", "black")
   }
 
 
@@ -97,7 +112,144 @@
     .join("circle")
       .attr("cx", d => d.x)
       .attr("cy", d => d.y)
-      .attr("r", 2)
-      .attr("fill", (d, i) => d3.schemeCategory10[5])
+      .attr("r", 2.5)
+      .attr("fill", "black")
 
   svg.select("#bezier").raise()
+
+
+var slider = svg.append("g")
+    .attr("class", "slider")
+    .attr("transform", "translate(" + margin.left + "," + height * 0.9 + ")");
+
+slider.append("text")
+      .attr("class","t")
+      .attr("transform", "translate(" + width / 2 + "," + 65 + ")")
+      .style("font", "25px sans-serif")
+      .style("text-anchor", "middle")
+      .style("user-select", "none")
+      .text("Iterate times: " + iter_times);
+
+var play_button = slider.append("g")
+      .attr("class", "button")
+      .attr("transform", "translate(0," + 40 + ")");
+
+play_button.append("text")
+        .attr("x", 35)
+        .attr("y", 22)
+        .style("font", "18px sans-serif")
+        .style("text-anchor", "middle")
+        .style("user-select", "none")
+        .text("Play");
+
+play_button.append("rect")
+      .attr('width', 70)
+      .attr('height', 30)
+      .attr('fill', "#ffad8a")
+      .attr("stroke", "black")
+      .style("opacity", 0.3)
+      .on("mousedown", function(){d3.select(this).attr('fill', "#6e6060")})
+      .on("mouseup", function(){d3.select(this).attr('fill', "#ffad8a")})
+      .on("click", onClick);
+
+slider.append("line")
+    .attr("class", "track-inset")
+    .attr("x1", x.range()[0])
+    .attr("x2", handle_position[0].x)
+    .attr("y1", 0)
+    .attr("y2", 0)
+    .attr("stroke", "#3b99fc")
+    .attr("stroke-width", "10px")
+    .attr("stroke-linecap", "round");
+
+slider.append("line")
+    .attr("class", "track-overlay")
+    .attr("x1", handle_position[0].x)
+    .attr("x2", x.range()[1])
+    .attr("y1", 0)
+    .attr("y2", 0)
+    .attr("stroke", "#cccccc")
+    .attr("stroke-width", "10px")
+    .attr("stroke-linecap", "round");
+
+slider.insert("g")
+    .attr("transform", "translate(0," + 25 + ")")
+  .selectAll("text")
+  .data(x.ticks(7))
+  .enter().append("text")
+    .style("font", "15px sans-serif")
+    .attr("x", x)
+    .style("text-anchor", "middle")
+    .style("user-select", "none")
+    .text(function(d) { return d; });
+
+var handle = slider.selectAll("circle")
+    .data(handle_position)
+    .join("circle")
+      .attr("cx", d => d.x)
+      .attr("cy", d => d.y)
+      .attr("r", 8)
+      .attr("fill", "white")
+      .attr("stroke", "gray")
+      .call(d3.drag()
+      .on("start", dragstarted)
+      .on("drag", handle_dragged)
+      .on("end", handle_dragended)
+      .on("start.update drag.update end.update", handle_update));
+
+function handle_dragged(event, d) {
+    d3.select(this).attr("cx", d.x = event.x);
+    if(event.x < 0){
+      d3.select(this).attr("cx", d.x = 0);
+    }else if(event.x > width){
+      d3.select(this).attr("cx", d.x = width);
+    }
+}
+
+function handle_dragended(event, d) {
+    d3.select(this).attr("stroke", "#cccccc");
+    handle_position[0].x = (iter_times - 3) / 7 * width;
+    handle.attr("cx", handle_position[0].x);
+    slider.select(".track-inset").attr("x2", handle_position[0].x);
+    slider.select(".track-overlay").attr("x1", handle_position[0].x);
+    Shiny.setInputValue(
+        "circle_clicked",
+        circles,
+        {priority: "event"}
+        );
+}
+
+function handle_update() {
+  var old_iter_times = iter_times;
+  iter_times = Math.round(handle_position[0].x / width * 7 + 3);
+  slider.select(".track-inset").attr("x2",handle_position[0].x);
+  slider.select(".track-overlay").attr("x1",handle_position[0].x);
+  slider.select(".t").text("Iterate times: " + iter_times);
+  if(old_iter_times != iter_times){
+    update();
+  }
+}
+
+function onClick(){
+  if(play_button.select("text").text() == "Play"){
+    play_button.select("text").text("Pause");
+    timer = d3.interval(time_update, 1000);
+  } else{
+    play_button.select("text").text("Play");
+    timer.stop();
+  }
+}
+
+function time_update(){
+  iter_times += 1;
+  iter_times = iter_times % 11;
+  if (iter_times < 3){
+    iter_times = 3;
+  }
+  handle_position[0].x = (iter_times - 3) / 7 * width;
+  slider.select(".track-inset").attr("x2", handle_position[0].x);
+  slider.select(".track-overlay").attr("x1", handle_position[0].x);
+  slider.select(".t").text("Iterate times: " + iter_times);
+  handle.attr("cx", handle_position[0].x);
+  update();
+}
